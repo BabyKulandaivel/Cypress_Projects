14b42531632c572dc2abdd0f35b49a47438aed86
BabyKulandaivel <110175732+BabyKulandaivel@users.noreply.github.com>
2023-09-25 16:09:04 +0530

    Modified

--- SignUp.cy.js
+++ SignUp.cy.js
@@ -16,8 +16,8 @@ describe('SignupFlows',()=>{
     cy.get('input[placeholder="Email"]').type('SSBK5@check.com');
     cy.get('input[placeholder="Password"]').type("123456");
     cy.get('button[type="submit"]').click();
-    cy.get('.error-messages > :nth-child(1)').contains('email has already been taken');
-    cy.get('.error-messages > :nth-child(2)').contains('username has already been taken');
+    cy.get('.error-messages > :nth-child(1)').should('have.text','email has already been taken');
+    cy.get('.error-messages > :nth-child(2)').should('have.text','username has already been taken');
 
    }) 
 
