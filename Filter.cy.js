8fd38561cbd7657b9003bd7c763c2e321b561eb5
BabyKulandaivel <110175732+BabyKulandaivel@users.noreply.github.com>
2023-09-26 16:43:45 +0530

    Modified

--- Filter.cy.js
+++ Filter.cy.js
@@ -3,6 +3,9 @@ describe('Filter lists', ()=>{
     it('Filter listes based on entered characters',()=>{
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
         cy.get('Input.search-keyword').type('ca');
-        cy.get('.product:visible').should('have.length',4);
+        //To find only visible elements
+        //cy.get('.product:visible').should('have.length',4);
+        cy.get('.products').find('.product').should('have.length',4);
+        cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click();
     })
 })
